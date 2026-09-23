d161ec7219b13a5cdf625586c9598ff709a5fa05
Vansh Garg <vanshgarg0706@gmail.com>
2026-09-23 14:12:12 +0530

    Updated Inventory and presentation

--- script.js
+++ script.js
@@ -10,31 +10,31 @@ const LOCALITIES = [
   {
     name: "Vasant Vihar",
     properties: [
-      { code:"A - 16/5 Vasant Vihar", tags:[["Ground Floor with Basement","2nd Floor","400 sq yards"],["South Facing"]], age:"Old Resale (10-12 months)", locationUrl:"https://www.google.com/maps/place/16,+A-9+St,+Block+A,+Vasant+Vihar,+New+Delhi,+Delhi+110057/@28.5641599,77.1622793,17z/data=!3m1!4b1!4m5!3m4!1s0x390d1da4647ba4dd:0xfdcf454749a041ae!8m2!3d28.5641552!4d77.1648542?entry=ttu&g_ep=EgoyMDI2MDkxNS4wIKXMDSoASAFQAw%3D%3D" },
-      { code:"F - 3/16 Vasant Vihar", tags:[["Ground Floor with Basement","400 sq yards","Third Floor with Terrace"],["West Facing"]], age:"Old Resale (2-4 months)", locationUrl:"#" },
-      { code:"B - 7/1B Vasant Vihar", tags:[["Second Floor","280 sq yards","Park Facing"],["North East Facing","BOOKING"]], age:"Old Resale (12–14 months)", locationUrl:"#" },
-      { code:"C - 1/9 Vasant Vihar", tags:[["Third Floor with Terrace","600 sq yards"],["Corner","Old Construction","West Facing"]], age:"Old Resale", locationUrl:"#" },
-      { code:"B - 1/27 Vasant Vihar", tags:[["Second Floor","400 sq yards"],["North East Facing","Prebooking"]], age:"Old Resale (18–20 months)", locationUrl:"#" },
-      { code:"D - 1/22 Vasant Vihar", tags:[["First Floor"],["North East Facing","Old Construction"]], age:"Old Resale (18–20 months)", locationUrl:"#" },
+      { code:"A - 16/5 Vasant Vihar", tags:[["Ground Floor with Basement","2nd Floor","400 sq yards"],["South Facing"]], age:"Old Resale (10-12 months)", locationUrl:"https://www.google.com/maps/place/28%C2%B033'38.3%22N+77%C2%B010'03.6%22E/@28.5606461,77.1651039,728m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d28.5606461!4d77.1676788!5m1!1e2?hl=en&entry=ttu&g_ep=EgoyMDI2MDkyMC4wIKXMDSoASAFQAw%3D%3D", presentation: "https://drive.google.com/file/d/1xb5bNqtT_QTvcjGQklRpzQys3apd9xQ7/view?usp=sharing" },
+      { code:"F - 3/16 Vasant Vihar", tags:[["Ground Floor with Basement","400 sq yards","Third Floor with Terrace"],["West Facing"]], age:"Old Resale (2-4 months)", locationUrl:"#", presentation: "https://drive.google.com/file/d/1weTMvDWfY0FbQCNIaYJlpImzNEBI92rv/view?usp=sharing" },
+      { code:"B - 7/1B Vasant Vihar", tags:[["Second Floor","280 sq yards","Park Facing"],["North East Facing","BOOKING"]], age:"Old Resale (12–14 months)", locationUrl:"https://www.google.com/maps/place/28%C2%B033'56.5%22N+77%C2%B009'43.6%22E/@28.5657043,77.1595421,728m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d28.5657043!4d77.162117!5m1!1e2?hl=en&entry=ttu&g_ep=EgoyMDI2MDkyMC4wIKXMDSoASAFQAw%3D%3D", presentation: "https://drive.google.com/file/d/1zDZGQ_emKB2Aev27QAfPBuZSCVJkhx5S/view?usp=sharing" },
+      { code:"C - 1/9 Vasant Vihar", tags:[["Third Floor with Terrace","600 sq yards"],["Corner","Old Construction","West Facing"]], age:"Old Resale", locationUrl:"#", presentation: "#" },
+      { code:"B - 1/27 Vasant Vihar", tags:[["Second Floor","400 sq yards"],["North East Facing","Prebooking"]], age:"Old Resale (18–20 months)", locationUrl:"#", presentation: "#" },
+      { code:"D - 1/22 Vasant Vihar", tags:[["First Floor"],["North East Facing","Old Construction"]], age:"Old Resale (18–20 months)", locationUrl:"#", presentation: "#" },
     ]
   },
   {
     name: "Anand Niketan",
     properties: [
-      { code:"C - 57 Anand Niketan", tags:[["First Floor","Second Floor","Third Floor with Terrace"],["400 sq yards","Park Facing","Booking","South East Facing"]], age:"Old Resale  (16–18 months)", locationUrl:"#" },
-      { code:"C - 34 Anand Niketan", tags:[["Ground Floor","400 sq yards","Newly Renovated"],["Park Facing","25-Year-Old Construction","North Facing"]], age:"Old Resale (25 years)", locationUrl:"#" },
+      { code:"C - 57 Anand Niketan", tags:[["First Floor","Second Floor","Third Floor with Terrace"],["400 sq yards","Park Facing","Booking","South East Facing"]], age:"Old Resale  (16–18 months)", locationUrl:"https://www.google.com/maps/place/28%C2%B034'40.8%22N+77%C2%B009'49.0%22E/@28.5779972,77.1610298,728m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d28.5779972!4d77.1636047!5m1!1e2?hl=en&entry=ttu&g_ep=EgoyMDI2MDkyMC4wIKXMDSoASAFQAw%3D%3D", presentation: "#" },
+      // { code:"C - 34 Anand Niketan", tags:[["Ground Floor","400 sq yards","Newly Renovated"],["Park Facing","25-Year-Old Construction","North Facing"]], age:"Old Resale (25 years)", locationUrl:"#" },
     ]
   },
   {
     name: "Greater Kailash - 1 (GK-1)",
     properties: [
-      { code:"E - 25 Greater Kailash-1", tags:[["First Floor","Basement & Ground Floor","300 sq yards","BOOKING"],["North Facing"]], age:"Old Resale (14–16 months)", locationUrl:"#" },
+      { code:"E - 25 Greater Kailash-1", tags:[["First Floor","Basement & Ground Floor","300 sq yards","BOOKING"],["North Facing"]], age:"Old Resale (14–16 months)", locationUrl:"#", presentation: "#" },
     ]
   },
   {
     name: "Shanti Niketan",
     properties: [
-      { code:"4/9, Shanti Niketan", tags:[["Ground Floor with Basement","North East Facing","10 year Old Construction"],["North Facing","Front Portion"]], age:"Old Resale (10 years)", locationUrl:"#" },
+      { code:"4/9, Shanti Niketan", tags:[["Ground Floor with Basement","North East Facing","10 year Old Construction"],["North Facing","Front Portion"]], age:"Old Resale (10 years)", locationUrl:"#", presentation: "#" },
     ]
   }
 ];
@@ -64,7 +64,7 @@ function renderGrid(){
             <h3 class="p-title">${p.code}</h3>
             <div class="tag-groups">${tagRowsHTML(p.tags, false)}</div>
             <div class="link-list">
-              <div class="link-item action">${ICON_PRESENTATION}<span>Site Presentation</span></div>
+              <div class="link-item action">${ICON_PRESENTATION}<span><a href="${loc.properties.presentation}">Site Presentation</a></span></div>
               <div class="link-item loc">${ICON_PIN}<a href="${p.locationUrl}" target="_blank" rel="noopener">Click here for Location</a></div>
               <div class="link-item info">${ICON_BUILDING}<span>${p.age}</span></div>
             </div>
